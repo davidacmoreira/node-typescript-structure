00e086f4b79cce8fb481f224f64e659014dd6b48
David Moreira <davidmoreira37@gmail.com>
2020-06-20 09:31:42 +0100

    Add TypeScript configurations and paths

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import "@config/config.json";
 
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' });
+  return response.json({ message: 'Hello World' });
 });
 
 app.listen(3333);
